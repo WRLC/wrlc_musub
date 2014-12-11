bcad0a3082481068da351c28290bb45ee26f49eb
MorganDawe <mdawe@discoverygarden.ca>
2014-12-11 09:42:45 -0400

    Refactored drupal behaviour in theme.

--- js/script.js
+++ js/script.js
@@ -14,52 +14,49 @@
 
 
 // To understand behaviors, see https://drupal.org/node/756722#behaviors
-Drupal.behaviors.my_custom_behavior = {
+Drupal.behaviors.wrlc_primary_islandora_solr_simple_search_text = {
   attach: function(context, settings) {
-    $('#islandora-solr-simple-search-form input.form-text', context).once('my_custom_behavior', function() {
-      if ($(this).val() == Drupal.t('')) $(this).val('Search...');
-      $(this).focus(function() {
-        if ($(this).val() == '') {
-          $(this).val(Drupal.t('Search...'));
-        }
-        else {
-          $(this).val(Drupal.t(''));
+    $('#islandora-solr-simple-search-form input.form-text', context).once('wrlc_primary_islandora_solr_simple_search_text', function() {
+      // Set an initial default value for the search box
+      if ($(this).val() == '') $(this).val(Drupal.t('Search...'));
+      // Add focusin handler to clear the search box.
+      $(this).focusin(function() {
+        if ($(this).val() == Drupal.t('Search...')) {
+          $(this).val('');
         }
       });
+      // Return search box default if it is empty.
       $(this).focusout(function() {
         if ($(this).val() == '') {
           $(this).val(Drupal.t('Search...'));
         }
-        else {
-          $(this).val(Drupal.t(''));
-        }
       });
       }
     );
+
+    // Sticky footer
+    function positionFooter() {
+      var mFoo = $("#footer");
+      if ((($(document.body).height() + mFoo.outerHeight()) < $(window).height() && mFoo.css("position") == "fixed") || ($(document.body).height() < $(window).height() && mFoo.css("position") != "fixed")) {
+        mFoo.css({
+          position: "fixed",
+          bottom: "0px"
+        });
+      }
+      else {
+        mFoo.css({
+          position: "static"
+        });
+      }
+    }
+    
     // Fix the footer position on AJAX callbacks.
     positionFooter();
+
+    // Fix footer position on window events.
+    $(window).scroll(positionFooter);
+    $(window).resize(positionFooter);
+    $(window).load(positionFooter);
   }
 };
-
-  // Sticky footer
-  function positionFooter() {
-    var mFoo = $("#footer");
-    if ((($(document.body).height() + mFoo.outerHeight()) < $(window).height() && mFoo.css("position") == "fixed") || ($(document.body).height() < $(window).height() && mFoo.css("position") != "fixed")) {
-      mFoo.css({
-        position: "fixed",
-        bottom: "0px"
-      });
-    }
-    else {
-      mFoo.css({
-        position: "static"
-      });
-    }
-  }
-  $(document).ready(function () {
-      positionFooter();
-      $(window).scroll(positionFooter);
-      $(window).resize(positionFooter);
-      $(window).load(positionFooter);
-  });
 })(jQuery, Drupal, this, this.document);
