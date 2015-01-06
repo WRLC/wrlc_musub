4d2a0c7e4dd41715a75bada660caadc9603daf51
MorganDawe <mdawe@discoverygarden.ca>
2015-01-06 14:06:34 -0400

    Addressed code review feedback and brought theme up to coder standards.

--- theme-settings.php
+++ theme-settings.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Implements hook_form_system_theme_settings_alter().
- *
- * @param $form
- *   Nested array of form elements that comprise the form.
- * @param $form_state
- *   A keyed array containing the current state of the form.
+ * @file
+ * Contains the theme settings file hook implementations and customizations.
  */
-function wrlc_primary_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL)  {
+
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ */
+function wrlc_primary_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL) {
   // Work-around for a core bug affecting admin themes. See issue #943212.
   if (isset($form_id)) {
     return;
